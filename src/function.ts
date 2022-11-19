3462494ee1b1ecbf247b51c7e8db9545e2533912
vickyvaf <vickyadi243@gmail.com>
2022-11-19 21:28:02 +0700

    function

---
+++ src/function.ts
@@ -0,0 +1,13 @@
+// Tipe data pada balikan function
+
+function getName(): string {
+  return "Vicky";
+}
+
+function getAge(): number {
+  return 18;
+}
+
+function printName(): void {
+  console.log("my name is Vicky");
+}
