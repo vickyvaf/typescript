b2ee558b4322042b200c692768e59d5b60658d55
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2023-01-02 13:08:16 +0700

    06

---
+++ src/06-unions.problems.ts
@@ -0,0 +1,29 @@
+// Delineate Types a Value Can Be
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  /**
+   * How do we ensure that role is only one of:
+   * - 'admin'
+   * - 'user'
+   * - 'super-admin'
+   */
+
+  /**
+   * Menambahkan Union supaya ada pilihan bisa di buat terpisah dengan membuat types
+   */
+  role: Role;
+}
+
+type Role = "admin" | "user" | "super-admin";
+
+export const defaultUser: User = {
+  id: 1,
+  firstName: "vicky",
+  lastName: "adi",
+  role: "super-admin",
+};
+
+console.log(defaultUser);
