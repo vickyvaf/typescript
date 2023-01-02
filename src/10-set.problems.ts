4c74db7e0302cdcd9b41da3ff03840dc60a7974d
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2023-01-02 14:35:23 +0700

    10

---
+++ src/10-set.problems.ts
@@ -0,0 +1,10 @@
+//  Passing Type Arguments
+
+// Menambahkan type di set
+
+const guitarists = new Set<string>();
+
+guitarists.add("Jimi Hendrix");
+guitarists.add("Eric Clapton");
+
+// guitarists.add(1) -> Error isn't string
