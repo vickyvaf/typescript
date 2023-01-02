df9e152817182c513c3553820b5f1ce3c813bdf0
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2023-01-02 12:23:14 +0700

    add three solution

--- src/02-object-params.problems.ts
+++ src/02-object-params.problems.ts
@@ -1,6 +1,27 @@
 // Working with Object Params
 
-const objectParams = (params: { first: number; second: number }) => {
+// First Solution
+// const objectParams = (params: { first: number; second: number }) => {
+//   return params.first + params.second;
+// };
+
+// Second Solution adding type
+// type ObjectParams = {
+//   first: number;
+//   second: number;
+// };
+
+// const objectParams = (params: ObjectParams) => {
+//   return params.first + params.second;
+// };
+
+// Third Solution adding interface
+interface ObjectParams {
+  first: number;
+  second: number;
+}
+
+const objectParams = (params: ObjectParams) => {
   return params.first + params.second;
 };
 
