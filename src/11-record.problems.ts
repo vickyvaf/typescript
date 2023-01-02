724d0529914b831577bc71d0ab851bce0c915d86
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2023-01-02 14:49:27 +0700

    11

---
+++ src/11-record.problems.ts
@@ -0,0 +1,26 @@
+//  Assigning Dynamic Keys to an Object
+
+const createCache = () => {
+  const cache: Record<string, string> = {};
+
+  const add = (id: string, value: string) => {
+    cache[id] = value;
+  };
+
+  const remove = (id: string) => {
+    delete cache[id];
+  };
+
+  return {
+    cache,
+    add,
+    remove,
+  };
+};
+
+const cache = createCache();
+
+cache.add("1", "LOL");
+cache.add("2", "LOL2")
+
+console.log(cache.cache[2]);
